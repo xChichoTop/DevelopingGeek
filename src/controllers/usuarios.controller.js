32f803b4ca6178913d0f3895a4c3909ab0dbd406
exelCode226 <exelagualimpia@gmail.com>
2023-08-11 14:01:37 -0500

    Proyecto con encarpetado Base

---
+++ src/controllers/usuarios.controller.js
@@ -0,0 +1,26 @@
+import usuarioModels from "../models/usuario.models.js"
+
+export const register =async (req, res) => {
+    const { nombre, apellidos, email, edad, cargo, password } = req.body
+    console.log(nombre, apellidos, email, edad, cargo, password)
+
+    try {
+        const newUsuario = new usuarioModels({
+            nombre,
+            apellidos,
+            email,
+            edad,
+            cargo,
+            password
+        })
+        await newUsuario.save()
+        res.send(newUsuario)
+    } catch (error) {
+        console.log(error)
+    }
+   
+}
+
+export const login = (req, res) => {
+    res.send("login")
+}
